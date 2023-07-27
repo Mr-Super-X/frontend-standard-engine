f16aa89f3625e1a8e6d1fd0fa0ffa8ab4524fc47
v_jpch <v_jpch@digitalgd.com.cn>
2023-07-27 15:49:43 +0800

    feat(other): 新增husky配置

---
+++ lint-staged.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [],
+  "{!(package)*.json,*.code-snippets,.!(browserslist)*rc}": [],
+  "package.json": [],
+  "*.vue": [],
+  "*.{scss,less,styl,html}": [],
+  "*.md": []
+}
